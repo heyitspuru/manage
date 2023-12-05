da79011356180de36b66dcbb2530db88827e7ae9
heyitspuru <143201274+heyitspuru@users.noreply.github.com>
2023-12-05 15:19:45 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '54.235.28.155', port: 8000 }
 
 end
